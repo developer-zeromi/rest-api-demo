41bd1e29bf0eddda11c4cfc9e795c4dda02a96db
youngmi <devzeromi@gmail.com>
2024-03-26 19:46:58 +0900

    fail on unknown properties

--- src/test/java/me/youngmi/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/youngmi/restapiwithspring/events/EventControllerTests.java
@@ -36,6 +36,36 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2024, 03, 13, 22, 05))
+                .closeEnrollmentDateTime(LocalDateTime.of(2024, 03, 14, 22, 05))
+                .beginEventDateTime(LocalDateTime.of(2024, 03, 15, 22, 05))
+                .endEventDateTime(LocalDateTime.of(2024, 03, 16, 22, 05))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events") // 요청
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print()) // 요청과 응답 출력
+                .andExpect(status().isCreated()) // 응답 확인 201 CREATED
+                .andExpect(jsonPath("id").exists()) // id 확인
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100))) // 직접 입력한 값이 들어오면 안됨
+                .andExpect(jsonPath("free").value(Matchers.not(true))) // 서비스가 계산 한 값이 들어와야 함
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name())) // 로직이 연산한 값이 들어와야 함
+        ;
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -58,13 +88,7 @@ public class EventControllerTests {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print()) // 요청과 응답 출력
-                .andExpect(status().isCreated()) // 응답 확인 201 CREATED
-                .andExpect(jsonPath("id").exists()) // id 확인
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100))) // 직접 입력한 값이 들어오면 안됨
-                .andExpect(jsonPath("free").value(Matchers.not(true))) // 서비스가 계산 한 값이 들어와야 함
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name())) // 로직이 연산한 값이 들어와야 함
+                .andExpect(status().isBadRequest()) // 응답 확인 400 BAD REQUEST
         ;
     }
 
